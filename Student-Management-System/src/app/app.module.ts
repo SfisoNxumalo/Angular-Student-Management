153cc5bdead0340a16b7403d2b83313b879ce98e
Sifiso Mawila <sfisonxumalo69@gmail.com>
2023-09-13 16:12:41 +0200

    Updated

--- Student-Management-System/src/app/app.module.ts
+++ Student-Management-System/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchStudentComponent } from './search-student/search-student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchStudentComponent
   ],
   imports: [
     BrowserModule,
