08104200f60d983114350614c66627473919d071
realebogamatlakala <real.matlakala96@gmail.com>
2023-09-13 16:48:35 +0200

    student-detail-view

--- Student-Management-System/src/app/app-routing.module.ts
+++ Student-Management-System/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentDetailsComponent } from './student-details/student-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: "student-details", component: StudentDetailsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Student-Management-System/src/app/app.module.ts
+++ Student-Management-System/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentDetailsComponent } from './student-details/student-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentDetailsComponent
   ],
   imports: [
     BrowserModule,
