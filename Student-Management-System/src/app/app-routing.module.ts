a01c489db6ec53ef5df6e2d6bb6e55059112bbe6
Lilitha03 <lilithadokoda@gmail.com>
2023-09-13 17:02:17 +0200

    add component

--- Student-Management-System/src/app/app-routing.module.ts
+++ Student-Management-System/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentAddComponent } from './student-add/student-add.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:"add-student" ,component: StudentAddComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ Student-Management-System/src/app/student-add/student-add.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-add',
+  templateUrl: './student-add.component.html',
+  styleUrls: ['./student-add.component.css']
+})
+export class StudentAddComponent {
+
+}
