39def60fe10a02c51cdf752e1382da86dd8aafea
VickyDana <uviwedana09@gmail>
2023-09-14 11:56:47 +0200

    adding student edit component

--- Student-Management-System/src/app/student.service.ts
+++ Student-Management-System/src/app/student.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const student_api = ""
+ 
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
 
-  constructor() { }
+  constructor(
+
+    private http: HttpClient
+
+  ) { }
+
+  getStudent(data: any, id: any){
+    return this.http.get(student_api+id)
+  }
+  updateStudents(data: any, id: any){
+    return this.http.put(student_api+id, data)
+  }
+
+  getAllStudent(){
+    
+    return this.http.get(student_api, httpOptions)
+  }
 }
+createStudent(data: any){
+
+  return this.http.post(student_api, httpOptions)
+}
+
+
+
+
+
+deleteStudent(studentid: any, id: any){
+  return this.http.delete(student_api + `${studentid}/${id}`, httpOptions)
+}
+
+
+
+
+
+
+
+@
