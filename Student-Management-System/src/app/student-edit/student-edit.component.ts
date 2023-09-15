fd2b2c953dc2ff5319d6138002fe01c9ec2fd523
VickyDana <uviwedana09@gmail>
2023-09-15 14:53:22 +0200

    updates

--- Student-Management-System/src/app/student-edit/student-edit.component.ts
+++ Student-Management-System/src/app/student-edit/student-edit.component.ts
@@ -1,39 +1,55 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'; // Import ActivatedRoute
+import { StudentService } from '../student.service';
 
 @Component({
   selector: 'app-student-edit',
   templateUrl: './student-edit.component.html',
   styleUrls: ['./student-edit.component.css']
 })
-export class StudentEditComponent  {
+export class StudentEditComponent implements OnInit {
 
-  studentDetails = {
+  student: any;
+  id: string;
 
-      studentNumber: "",
-      firstName: "",
-      lastName: "",
-      email: "",
-      phoneNumber: "",
-      address: "",
-      dateOfBirth: "",
-      course: ""
-      
+  constructor(
+    private studentService: StudentService, // Use correct service name
+    private route: ActivatedRoute // Inject ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    // Assuming 'id' comes from the route parameter
+    // this.studentServ,this.id = this.route.snapshot.paramMap.get("id");
+    this.id =this.studentService("id")
+    this.getStudent();
+  }
+
+  getStudent() {
+    this.studentService.getStudent(this.student,this.id).subscribe({
+      next: (data) => {
+        this.student = data;
+        console.log(this.student);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
+
+  updateStudents() {
+    this.studentService.updateStudents(this.student, this.id).subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
 }
 
 
-student: any [] = []
 
-onSave(){
-
-  
-console.log(this.studentDetails)
-this.student.push(this.studentDetails)
-// console.log(this.student)
-
-}
-
-}
 
 
 
