1603e3a6a7b1713e3dc019dc518695abf4f9cc10
VickyDana <uviwedana09@gmail>
2023-09-18 09:30:08 +0200

    updates

--- Student-Management-System/src/app/student-edit/student-edit.component.ts
+++ Student-Management-System/src/app/student-edit/student-edit.component.ts
@@ -7,46 +7,12 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   templateUrl: './student-edit.component.html',
   styleUrls: ['./student-edit.component.css']
 })
-export class StudentEditComponent implements OnInit {
+export class StudentEditComponent  {
 
-  constructor(private serviceStudents : StudentService){}
 
-  studentDetails = {
 
-  constructor(
-    private studentService: StudentService, // Use correct service name
-    private route: ActivatedRoute // Inject ActivatedRoute
-  ) {}
 
-  ngOnInit() {
-    // Assuming 'id' comes from the route parameter
-    // this.studentServ,this.id = this.route.snapshot.paramMap.get("id");
-    this.id =this.studentService("id")
-    this.getStudent();
-  }
 
-  getStudent() {
-    this.studentService.getStudent(this.student,this.id).subscribe({
-      next: (data) => {
-        this.student = data;
-        console.log(this.student);
-      },
-      error: (error) => {
-        console.error(error);
-      }
-    });
-  }
-
-  updateStudents() {
-    this.studentService.updateStudents(this.student, this.id).subscribe({
-      next: (data) => {
-        console.log(data);
-      },
-      error: (error) => {
-        console.error(error);
-      }
-    });
-  }
 }
 
 
@@ -58,11 +24,9 @@ export class StudentEditComponent implements OnInit {
 
 // onSave(){
 
-\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-  
 // console.log(this.studentDetails)
 // this.student.push(this.studentDetails)
 // // console.log(this.student)
 
 // }
-}
+
