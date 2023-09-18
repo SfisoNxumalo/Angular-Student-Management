95653a6c4c9c6c87ee6a49a4385fc132cf41611a
Sifiso Mawila <sfisonxumalo69@gmail.com>
2023-09-18 12:39:07 +0200

    updated

--- Student-Management-System/src/app/student-edit/student-edit.component.ts
+++ Student-Management-System/src/app/student-edit/student-edit.component.ts
@@ -15,7 +15,7 @@ student?: any;
   editable = true;
   
   ngOnInit(): void {
-    this.getOne()
+    this.getOne();
   }
 
   isEditable(){
@@ -26,25 +26,24 @@ student?: any;
 
   studentDetails = {
 
-      studentNumber: this.student.studentNumber,
-      firstName: this.student.firstName,
-      lastName: this.student.lastName,
-      email: this.student.email,
-      phoneNumber: this.student.phoneNumber,
-      address: this.student.address,
-      dateOfBirth: this.student.dateOfBirth,
-      course: this.student.course
-      
+    studentNumber: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    address: "",
+    dateOfBirth: "",
+    course: ""
+    
 }
-
-
 
 getOne(){
   const id = this.route.snapshot.params[('id')]
   this.serviceStudents.getStudent(id).subscribe({
     next: (data)=>{
       this.student = data
-      console.log(data)
+      this.studentDetails = this.student[0]
+      
     },
     error: (e)=> console.log(e)
   })
